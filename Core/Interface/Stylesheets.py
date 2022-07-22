f2c31b9fbbb53c65917d80eda5eb75d8bce0f778
AccentuSoft <socials@accentusoft.com>
2022-07-22 11:35:19 +0300

    Make headers have the correct color. Improve Stylesheets.py formatting. Remove redundant stylesheet.

--- Core/Interface/Stylesheets.py
+++ Core/Interface/Stylesheets.py
@@ -16,82 +16,97 @@ TOOLBAR_STYLESHEET = """QToolBar {background-color: rgb(41, 45, 62);
 
 MAIN_WINDOW_STYLESHEET = """
                         QWidget{ 
-                        background-color: rgb(41, 45, 62);
-                        color: rgba(248, 248, 242, 1) !important;
-                        font-size: 15px;
+                         background-color: rgb(41, 45, 62);
+                         color: rgba(248, 248, 242, 1) !important;
+                         font-size: 15px;
                         }
+
                         QScrollBar:vertical {         
-                        background:rgb(44, 49, 58);
-                        width:7px;    
-                        margin: 0px 0px 0px 0px;
+                         background:rgb(44, 49, 58);
+                         width:7px;    
+                         margin: 0px 0px 0px 0px;
                         }
+
                         QScrollBar::handle:vertical {
-                            background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
-                            stop: 0 rgb(103, 110, 149), stop: 0.5 rgb(103, 110, 149), stop:1 rgb(103, 110, 149));
-                            min-height: 0px;
+                         background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
+                         stop: 0 rgb(103, 110, 149), stop: 0.5 rgb(103, 110, 149), stop:1 rgb(103, 110, 149));
+                         min-height: 0px;
                         }
+
                         QScrollBar:horizontal {         
-                            background:rgb(44, 49, 58);
-                            height:7px;    
-                            margin: 0px 0px 0px 0px;
+                         background:rgb(44, 49, 58);
+                         height:7px;    
+                         margin: 0px 0px 0px 0px;
                         }
+
                         QScrollBar::handle:horizontal {
-                            background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
-                            stop: 0 rgb(103, 110, 149), stop: 0.5 rgb(103, 110, 149), stop:1 rgb(103, 110, 149));
+                         background: qlineargradient(x1:0, y1:0, x2:1, y2:0,
+                                                     stop: 0 rgb(103, 110, 149),
+                                                     stop: 0.5 rgb(103, 110, 149),
+                                                     stop:1 rgb(103, 110, 149));
                         }
                         QMenuBar { 
-                          color: #ffffff;
-                          background-color: rgb(33, 37, 43);
-                          font-size: 15px;
+                         color: #ffffff;
+                         background-color: rgb(33, 37, 43);
+                         font-size: 15px;
                         }
 
                         QPushButton {
-                        border: 2px solid rgb(0, 173, 238);
-                        border-radius: 6px;
-                        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-                                                          stop: 1 rgb(0, 173, 238), stop: 0.7 rgb(44, 49, 58));
-                        min-width: 50px;
+                         border: 2px solid rgb(0, 173, 238);
+                         border-radius: 6px;
+                         background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                                           stop: 1 rgb(0, 173, 238), stop: 0.7 rgb(44, 49, 58));
+                         min-width: 50px;
                         }
 
                         QLabel { 
-                        border: 2px solid rgb(41, 45, 62);
-                        padding-left: 7px;
-                        border-left-color: rgb(0, 173, 238);
+                         border: 2px solid rgb(41, 45, 62);
+                         padding-left: 7px;
+                         border-left-color: rgb(0, 173, 238);
                         }
                         
                         QLineEdit, QPlainTextEdit { 
-                        border: 0.5px solid rgb(0, 173, 238);
-                        border-radius: 6px;
+                         border: 0.5px solid rgb(0, 173, 238);
+                         border-radius: 6px;
                         }
                         
-                        QTabBar::tab {background: rgb(68, 66, 103);
-                        border: 2px solid rgb(41, 45, 62);
-                        border-radius: 3px;
-                        min-height: 3ex;
+                        QTabBar::tab {
+                         background: rgb(68, 66, 103);
+                         border: 2px solid rgb(41, 45, 62);
+                         border-radius: 3px;
+                         min-height: 3ex;
                         }
+
                         QTabWidget {
-                        border-style: outset;
-                        border-color: rgba(248, 248, 242, 1);
-                        border-width: 0.5px;
+                         border-style: outset;
+                         border-color: rgba(248, 248, 242, 1);
+                         border-width: 0.5px;
                         }
+
                         QToolBar {
-                        border-style: outset;
-                        border-color: rgba(75, 75, 75, 1);
-                        border-width: 1px;
-                        border-left-width: 0px;
-                        border-right-width: 0px;
+                         border-style: outset;
+                         border-color: rgba(75, 75, 75, 1);
+                         border-width: 1px;
+                         border-left-width: 0px;
+                         border-right-width: 0px;
                         }
                         
-                        QTabBar::tab:selected {background: rgb(51, 55, 95);
-                        border: 2px solid rgb(41, 45, 62);
-                        min-height: 2.5ex;
-                        border-radius: 3px;
-                        border-top-color: rgb(0, 173, 238);
+                        QTabBar::tab:selected {
+                         background: rgb(51, 55, 95);
+                         border: 2px solid rgb(41, 45, 62);
+                         min-height: 2.5ex;
+                         border-radius: 3px;
+                         border-top-color: rgb(0, 173, 238);
                         }
                         
                         QComboBox { combobox-popup: 0; }
                         
-                        QMenu::item{
+                        QHeaderView::section { 
+                         background-color: rgb(41, 45, 62);
+                         color: rgba(248, 248, 242, 1);
+                        }
+                        
+                        QMenu::item {
                          background-color: rgb(41, 45, 62);
                          color: rgba(248, 248, 242, 1) !important;
                          border-left: 1px solid rgb(0, 173, 238);
@@ -101,16 +116,16 @@ MAIN_WINDOW_STYLESHEET = """
                          font-size: 15px;
                          text-align: left;
                          padding-left: 4px;
-                         }
+                        }
                         
-                         QMenu::item:selected{
+                        QMenu::item:selected {
                          background-color: rgb(0, 85, 127);
                          color: rgb(255, 255, 255);
-                         }
+                        }
                          
-                         QMenu::item:disabled {
+                        QMenu::item:disabled {
                          background-color:rgb(81, 87, 114);
-                         }
+                        }
                         """
 
 DOCK_BAR_TWO_LINK = """
@@ -128,13 +143,6 @@ DOCK_BAR_LABEL = """
                                                           stop: 1 rgb(0, 173, 238), stop: 0.85 rgb(41, 45, 62));
                         }
                   """
-
-DROPDOWN_MENU_STYLE = "background-color: rgb(41, 45, 62);" \
-                      "color: rgba(248, 248, 242, 1) !important;" \
-                      "border: 2px solid rgb(44, 49, 58);" \
-                      "border-left-color: rgb(189, 147, 249);" \
-                      "font-family: Segoe UI;" \
-                      "font-size: 13px;"
 
 TEXT_BOX_STYLESHEET = "QLineEdit, QLabel{" \
                       "border-bottom: 1px solid rgb(0, 173, 238);" \
